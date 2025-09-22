9ab1638b9f0fbcd33d1cb97a6826df5e42d80a08
Pallavi Pradhan <pallavipradhan5578@gmail.com>
2025-09-23 00:51:26 +0530

    initial commit

---
+++ first.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+//     cout<<"Hello Pallu";
+//     int a,b; //variable declare
+//     cout<<"Enter your first num : ";
+//     cin>>a; //variable declare
+//     cout<<"Enter your second num : ";
+//     cin>>b;
+// cout<<"Sum of "<<a <<" and "<<b<< " is : "<<a+b<<endl;
+/// type casting
+int a=10;
+char b='c';
+a=b;
+cout<<a;
+
+
+
+}
---
+++ if_else.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int pacakge
+    // ;
+    // cin>>pacakge;
+    // if(pacakge>10){
+    //     cout<<"Offer accepted";
+    // }else{
+    //     cout<<"Offer rejected";
+   // }
+   int a , b;
+   cout<<"Enter Your a : ";
+   cin>>a; 
+   cout<<"Enter Your b : ";
+   cin>>b;
+   if(a>b){
+    cout<<"yes a is grater than b";
+
+   }else{
+     cout<<"  No a is less than b";
+   }
+}
