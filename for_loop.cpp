775a313790b90a88a2eca8b70935838a2cf9a320
Pallavi Pradhan <pallavipradhan5578@gmail.com>
2025-09-23 22:07:28 +0530

    fibonacci series.dart

--- for_loop.cpp
+++ for_loop.cpp
@@ -1,21 +1,21 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int i,n ;
-    cout<<"Enter your num : ";
-    cin>>n;
-    if(n<2){
-        cout<< n <<" is  Not a Prime number";
-        return 0;
-    }else{
-        for(i=2;i<n;i++){
-            if(n%i==0){
-               cout<< n <<" is  Not a Prime number";
-                return 0;   
-            }
-            
-        
-        }
-        cout<< n <<" is a Prime number";
-    }
+    int n,i,last,pre,curr;
+    last=0;pre=1;
+     cout<<"Enter  youe num :";
+     cin>>n;
+    cout<< last<<" "<< pre<<" ";
+    
+
+    
+    for ( i = 2; i < n; i++)
+    {
+        curr=last+pre;
+        cout<<curr<<" ";
+        last=pre;
+        pre=curr;
+       
+    }return 0;
+    
 }
