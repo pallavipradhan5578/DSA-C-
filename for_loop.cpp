96d23594dd78ca0d8f20e43fe419d66cc2a64a95
Pallavi Pradhan <pallavipradhan5578@gmail.com>
2025-09-23 16:04:39 +0530

    print alpabates ato z

--- for_loop.cpp
+++ for_loop.cpp
@@ -1,13 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
-    cout<<"Enter your num : ";
-    cin>>n;
-    for(int i=2;i<=n;i++){
-        if(i%2==0){
-            cout<<i<<endl;
-        }
+    char name;
+    for(name='a';name<='z';name=name+1)
+    {
+        cout<<name<<" " ;
     }
-
 }
