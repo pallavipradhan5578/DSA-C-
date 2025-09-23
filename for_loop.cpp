592a67d7321d9bf7bc382a3c0af7646982d9f4a7
Pallavi Pradhan <pallavipradhan5578@gmail.com>
2025-09-23 16:41:49 +0530

    multiplication table making

--- for_loop.cpp
+++ for_loop.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int  num;
-    cout<<"Enter the num : ";
+    int  num,i;
+    cout<<"Enter the table : ";
     cin>>num;
-    for(num=1;num<=100;num=num+3)
+    for( i=1;i<=10;i=i+1)
     {
-        cout<<num<<" " ;
+        cout<<num <<"*"<< i << " = "<<num*i<<endl ;
     }
 }
